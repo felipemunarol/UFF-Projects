fb2d998cf816b5a3a86c5359fb38ca6d4979c942
felipe <felipemunarolima@gmail.com>
2024-02-20 10:32:12 -0300

    creating python scripts

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='ors',
+	version='0.0.1',
+	install_requires=["click", "colorama"],
+	author="Felipe Munaro",
+	author_email="felipemunarolima@gmail.com"
+	packages=find_packages()
+)
+
+
